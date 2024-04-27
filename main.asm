d1f66ae5ca03460f028562dcf0c3b1cbae373011
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-04-27 13:53:52 +0800

    remove some spaces

--- main.asm
+++ main.asm
@@ -1,100 +1,100 @@
 incsrc header.asm
 incsrc .\macros\ines.asm
 incsrc definitions.asm
-bank = 0
+bank=0
 base $8000
 pad $a000
-bank = 1
+bank=1
 base $8000
 pad $a000
-bank = 2
+bank=2
 base $8000
 pad $a000
-bank = 3
+bank=3
 base $8000
 pad $a000
-bank = 4
+bank=4
 base $8000
 pad $a000
-bank = 5
+bank=5
 base $8000
 pad $a000
-bank = 6
+bank=6
 base $8000
 pad $a000
-bank = 7
+bank=7
 base $8000
 pad $a000
-bank = 8
+bank=8
 base $8000
 pad $a000
-bank = 9
+bank=9
 base $8000
 pad $a000
-bank = 10
+bank=10
 base $8000
 pad $a000
-bank = 11
+bank=11
 base $8000
 pad $a000
-bank = 12
+bank=12
 base $8000
 pad $a000
-bank = 13
+bank=13
 base $8000
 pad $a000
-bank = 14
+bank=14
 base $8000
 pad $a000
-bank = 15
+bank=15
 base $8000
 pad $a000
-bank = 16
+bank=16
 base $8000
 pad $a000
-bank = 17
+bank=17
 base $8000
 pad $a000
-bank = 18
+bank=18
 base $8000
 pad $a000
-bank = 19
+bank=19
 base $8000
 pad $a000
-bank = 20
+bank=20
 base $8000
 pad $a000
-bank = 21
+bank=21
 base $8000
 pad $a000
-bank = 22
+bank=22
 base $8000
 pad $a000
-bank = 23
+bank=23
 base $8000
 pad $a000
-bank = 24
+bank=24
 base $8000
 pad $a000
-bank = 25
+bank=25
 base $8000
 pad $a000
-bank = 26
+bank=26
 base $8000
 pad $a000
-bank = 27
+bank=27
 base $8000
 pad $a000
-bank = 28
+bank=28
 base $8000
 pad $a000
-bank = 29
+bank=29
 base $8000
 pad $a000
-bank = 30
+bank=30
 base $c000
 pad $e000
-bank = 31
+bank=31
 base $e000
 pad $fe00
 incsrc .\engine\reset.asm
