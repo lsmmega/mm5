48169a57a0178fe6be46d7de58909ac10b8aaca4
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-04-27 14:12:14 +0800

    remove some spaces

--- macros/ines.asm
+++ macros/ines.asm
@@ -1,28 +1,28 @@
-db "NES", $1a, prg_rom_units, chr_rom_units
+db "NES",$1a,prg_rom_units,chr_rom_units
 
 ;rom_mirroring
-horizontal = 0
-vertical = 1
-four_screen = 8
+horizontal=0
+vertical=1
+four_screen=8
 
-a = rom_mirroring
+a=rom_mirroring
 
 ;general
-no = 0
+no=0
 
 ;battery_backed
-yes = 2
-b = battery_backed
+yes=2
+b=battery_backed
 
 ;trained
-yes = 4
-c = trained
+yes=4
+c=trained
 
-;flags_6, mapper_lsb, rom_mirroring, battery_backed, trained
+;flags_6,mapper_lsb,rom_mirroring,battery_backed,trained
 db mapper%11110000*11110000+a+b+c
 
-;flags_7, mapper_msb, version = ines
+;flags_7,mapper_msb,version=ines
 db mapper&%11110000
 
 ;rarely_used
-db $00, $00, $00, $00, $00, $00, $00, $00
+db $00,$00,$00,$00,$00,$00,$00,$00
