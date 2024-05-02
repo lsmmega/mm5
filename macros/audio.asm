bf26d7127095ff01aa163667f7ef6e5c819561a2
lsmmega <pogmegalsm@outlook.com>
2024-05-02 20:02:01 +0800

    unfinished, note left

--- macros/audio.asm
+++ macros/audio.asm
@@ -38,3 +38,136 @@ else
 db 0, 0
 endif
 ENDM
+
+MACRO triplet_set
+db 0
+ENDM
+
+MACRO connect_set
+db 1
+ENDM
+
+MACRO dotted_set
+db 2
+ENDM
+
+MACRO flags b
+db 4, b
+ENDM
+
+MACRO tempo b
+db 5
+dh b
+dl b
+ENDM
+
+MACRO note_sustain_length b
+db 6, b
+ENDM
+
+MACRO volume b
+db 7, b
+ENDM
+
+MACRO instrument b
+db 8, b
+ENDM
+
+MACRO pitch_tune b
+db $c, b
+ENDM
+
+MACRO pitch_slide b
+db $d, b
+ENDM
+
+MACRO loop1 b, c
+db $e
+db c
+dh b
+dl b
+ENDM
+
+MACRO loop2 b, c
+db $f
+db c
+dh b
+dl b
+ENDM
+
+MACRO loop3 b, c
+db $10
+db c
+dh b
+dl b
+ENDM
+
+MACRO loop4 b, c
+db $11
+db c
+dh b
+dl b
+ENDM
+
+MACRO break1 b, c
+db $12
+db c
+dh b
+dl b
+ENDM
+
+MACRO break2 b, c
+db $13
+db c
+dh b
+dl b
+ENDM
+
+MACRO break3 b, c
+db $14
+db c
+dh b
+dl b
+ENDM
+
+MACRO break4 b, c
+db $15
+db c
+dh b
+dl b
+ENDM
+
+MACRO jump b
+db $16
+dh b
+dl b
+ENDM
+
+MACRO stop_playing
+db $17
+ENDM
+
+MACRO duty_cycle b
+db $18, b<<6
+ENDM
+
+MACRO rest b
+if b&1
+c = $20
+elseif b&2
+c = $40
+elseif b&4
+c = $60
+elseif b&8
+c = $80
+elseif b&16
+c = $a0
+elseif b&32
+c = $c0
+elseif b&64
+c = $e0
+else
+error "invaild note length"
+endif
+db c
+ENDM
