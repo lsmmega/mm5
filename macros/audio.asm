97c0c83d36f19d1c2911093d765d02e184e82755
lsmmega <pogmegalsm@outlook.com>
2024-05-06 20:26:24 +0800

    Use channel_flags and current note pitch and length_base_note to tell assembler which channel address set and relocate note

--- macros/audio.asm
+++ macros/audio.asm
@@ -3,6 +3,7 @@ db 0
 ENDM
 
 a = channel_flags
+b = current_base_note
 
 MACRO channel_1 c
 IF a&1
@@ -187,4 +188,135 @@ ELSE
 error "Invaild note length"
 ENDIF
 db d
+ENDM
+
+;note_value_table
+C_0 = $00
+C#0 = $01
+D_0 = $02
+D#0 = $03
+E_0 = $04
+F_0 = $05
+F#0 = $06
+G_0 = $07
+G#0 = $08
+A_0 = $09
+A#0 = $0a
+B_0 = $0b
+
+C_1 = $0c
+C#1 = $0d
+D_1 = $0e
+D#1 = $0f
+E_1 = $10
+F_1 = $11
+F#1 = $12
+G_1 = $13
+G#1 = $14
+A_1 = $15
+A#1 = $16
+B_1 = $17
+
+C_2 = $18
+C#2 = $19
+D_2 = $1a
+D#2 = $1b
+E_2 = $1c
+F_2 = $1d
+F#2 = $1e
+G_2 = $1f
+G#2 = $20
+A_2 = $21
+A#2 = $22
+B_2 = $23
+
+C_3 = $24
+C#3 = $25
+D_3 = $26
+D#3 = $27
+E_3 = $28
+F_3 = $29
+F#3 = $2a
+G_3 = $2b
+G#3 = $2c
+A_3 = $2d
+A#3 = $2e
+B_3 = $2f
+
+C_4 = $30
+C#4 = $31
+D_4 = $32
+D#4 = $33
+E_4 = $34
+F_4 = $35
+F#4 = $36
+G_4 = $37
+G#4 = $38
+A_4 = $39
+A#4 = $3a
+B_4 = $3b
+
+C_5 = $3c
+C#5 = $3d
+D_5 = $3e
+D#5 = $3f
+E_5 = $40
+F_5 = $41
+F#5 = $42
+G_5 = $43
+G#5 = $44
+A_5 = $45
+A#5 = $46
+B_5 = $47
+
+C_6 = $48
+C#6 = $49
+D_6 = $4a
+D#6 = $4b
+E_6 = $4c
+F_6 = $4d
+F#6 = $4e
+G_6 = $4f
+G#6 = $50
+A_6 = $51
+A#6 = $52
+B_6 = $53
+
+C_7 = $54
+C#7 = $55
+D_7 = $56
+D#7 = $57
+E_7 = $58
+F_7 = $59
+F#7 = $5a
+G_7 = $5b
+G#7 = $5c
+A_7 = $5d
+A#7 = $5e
+B_7 = $5f
+
+MACRO note c, d
+IF c < b
+error "Note is out of range"
+ELSEIF c - b > $1e
+error "Note is out of range"
+ENDIF
+IF d&1
+e = $21
+ELSEIF d&2
+e = $41
+ELSEIF d&4
+e = $61
+ELSEIF d&8
+e = $81
+ELSEIF d&16
+e = $a1
+ELSEIF d&32
+e = $c1
+ELSEIF d&64
+e = $e1
+ELSE
+error "Invaild note length"
+ENDIF
+db c - b + e
 ENDM
