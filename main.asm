702287c5c848e80f14b2976c3ea1da3c8ab435b2
lsmmega <pogmegalsm@outlook.com>
2024-05-24 17:32:43 +0800

    Update main.asm

--- main.asm
+++ main.asm
@@ -2,105 +2,105 @@ incsrc .\scripts\ines.asm
 incsrc .\scripts\audio.asm
 incsrc header.asm
 incsrc definitions.asm
-bank = 0
+prg_bank = 0
 base $8000
 pad $a000
-bank = 1
+prg_bank = 1
 base $8000
 pad $a000
-bank = 2
+prg_bank = 2
 base $8000
 pad $a000
-bank = 3
+prg_bank = 3
 base $8000
 pad $a000
-bank = 4
+prg_bank = 4
 base $8000
 pad $a000
-bank = 5
+prg_bank = 5
 base $8000
 pad $a000
-bank = 6
+prg_bank = 6
 base $8000
 pad $a000
-bank = 7
+prg_bank = 7
 base $8000
 pad $a000
-bank = 8
+prg_bank = 8
 base $8000
 pad $a000
-bank = 9
+prg_bank = 9
 base $8000
 pad $a000
-bank = 10
+prg_bank = 10
 base $8000
 pad $a000
-bank = 11
+prg_bank = 11
 base $8000
 pad $a000
-bank = 12
+prg_bank = 12
 base $8000
 pad $a000
-bank = 13
+prg_bank = 13
 base $8000
 pad $a000
-bank = 14
+prg_bank = 14
 base $8000
 pad $a000
-bank = 15
+prg_bank = 15
 base $8000
 pad $a000
-bank = 16
+prg_bank = 16
 base $8000
 pad $a000
-bank = 17
+prg_bank = 17
 base $8000
 pad $a000
-bank = 18
+prg_bank = 18
 base $8000
 pad $a000
-bank = 19
+prg_bank = 19
 base $8000
 pad $a000
-bank = 20
+prg_bank = 20
 base $8000
 pad $a000
-bank = 21
+prg_bank = 21
 base $8000
 pad $a000
-bank = 22
+prg_bank = 22
 base $8000
 pad $a000
-bank = 23
+prg_bank = 23
 base $8000
 pad $a000
-bank = 24
+prg_bank = 24
 base $8000
 pad $8a40
 incsrc .\audio\music_data_pointers.asm
 pad $a000
-bank = 25
+prg_bank = 25
 base $8000
 pad $a000
-bank = 26
+prg_bank = 26
 base $8000
 pad $a000
-bank = 27
+prg_bank = 27
 base $8000
 pad $a000
-bank = 28
+prg_bank = 28
 base $8000
 pad $a000
-bank = 29
+prg_bank = 29
 base $8000
 pad $a000
-bank = 30
+prg_bank = 30
 base $c000
 pad $c343
 incsrc .\engine\ppu_table_clear.asm
 incsrc .\engine\sprite_clear.asm
 pad $e000
-bank = 31
+prg_bank = 31
 base $e000
 pad $fe00
 incsrc .\engine\reset.asm
