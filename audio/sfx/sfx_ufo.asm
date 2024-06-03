03fc9f1d5b0a0406e964a05d4aeb431f8a512093
lsmmega <pogmegalsm@outlook.com>
2024-06-03 18:59:40 +0800

    Create sfx_ufo.asm

---
+++ audio/sfx/sfx_ufo.asm
@@ -0,0 +1,35 @@
+sfx_ufo:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_frames $0a
+	sfx_channel_flags $6
+	sfx_channel_command_flags $17
+	sfx_channel_command_flags_bits = $17
+	sfx_channel_instrument $3b
+	sfx_channel_duty_cycle 0
+	sfx_channel_volume $f
+	sfx_channel_pitch_tune $07
+	sfx_note D#7
+	sfx_channel_command_flags $05
+	sfx_channel_command_flags_bits = $05
+	sfx_channel_instrument $3b
+	sfx_channel_volume $ff
+	sfx_note D#6
+
+;2
+	sfx_global_flags $2
+    sfx_global_flags_bits = $2
+    sfx_global_note_sustain_length $ff
+    sfx_frames $0a
+    sfx_channel_flags $6
+	sfx_channel_command_flags $00
+    sfx_channel_command_flags_bits = $00
+	sfx_note D#7
+	sfx_channel_command_flags $04
+	sfx_channel_command_flags_bits = $04
+	sfx_channel_volume $00
+	sfx_note D#6
+	sfx_end
