f788a0f9bd66df125dcf6562b1f7cbad9be7203d
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-04 09:40:53 +0800

    Update sfx_lift_up.asm

--- audio/sfx/sfx_lift_up.asm
+++ audio/sfx/sfx_lift_up.asm
@@ -2,23 +2,23 @@ sfx_lift_up:
 ;1
 	sfx_priority $08
 	sfx_global_flags $2
-    sfx_global_flags_bits = $2
+	sfx_global_flags_bits = $2
 	sfx_global_note_sustain_length $ff
 	sfx_frames $64
 	sfx_channel_flags $a
-    sfx_channel_flags_bits = $a
+	sfx_channel_flags_bits = $a
 	sfx_pulse2_command_flags $0f
-    sfx_pulse2_command_flags_bits = $0f
+	sfx_pulse2_command_flags_bits = $0f
 	sfx_pulse2_instrument $28
 	sfx_pulse2_duty_cycle 2
 	sfx_pulse2_volume $e
 	sfx_pulse2_pitch_slide $8d
-	sfx_note D_1
+	sfx_pulse2_note D_1
 	sfx_noise_command_flags $05
-    sfx_noise_command_flags_bits = $05
-    sfx_noise_instrument $35
-    sfx_noise_volume $4
-    sfx_noise_note 7
+	sfx_noise_command_flags_bits = $05
+	sfx_noise_instrument $35
+	sfx_noise_volume $4
+	sfx_noise_note 7
 
 ;2
 sfx_lift_up_loop1:
@@ -34,7 +34,7 @@ sfx_lift_up_loop1:
 	sfx_pulse2_volume $9
 	sfx_pulse2_pitch_slide $00
 	sfx_pulse2_pitch_tune $0d
-	sfx_note G#2
+	sfx_pulse2_note G#2
 	sfx_noise_command_flags $04
 	sfx_noise_command_flags_bits = $04
 	sfx_noise_volume $4
