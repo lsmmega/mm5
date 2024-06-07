7d50d037a7cef48c3d0edf230afa7e329838156a
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-07 15:09:45 +0800

    Update sfx

---
+++ audio/sfx/sfx_pipe.asm
@@ -0,0 +1,29 @@
+sfx_pipe:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $cc
+	sfx_frames $05
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $07
+	sfx_pulse2_command_flags_bits = $07
+	sfx_pulse2_instrument $40
+	sfx_pulse2_duty_cycle 3
+	sfx_pulse2_volume $f
+	sfx_pulse2_note G#4
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_frames $05
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $06
+	sfx_pulse2_command_flags_bits = $06
+	sfx_pulse2_duty_cycle 1
+	sfx_pulse2_volume $2
+	sfx_pulse2_note G#4
+	sfx_end
