debb808ef88e1414e9cb7cc8bf7524db074d37ae
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-12 10:20:58 +0800

    Update sfx

---
+++ audio/sfx/lift_slow.asm
@@ -0,0 +1,119 @@
+sfx_lift_slow:
+;1
+	sfx_priority $08
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_frames $5a
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $1f
+	sfx_pulse2_command_flags_bits = $1f
+	sfx_pulse2_instrument $28
+	sfx_pulse2_duty_cycle 2
+	sfx_pulse2_volume $b
+	sfx_pulse2_pitch_slide $13
+	sfx_pulse2_pitch_tune $0d
+	sfx_pulse2_note G#2
+	sfx_noise_command_flags $0d
+	sfx_noise_command_flags_bits = $0d
+	sfx_noise_instrument $35
+	sfx_noise_volume $3
+	sfx_noise_pitch_slide $01
+	sfx_noise_note 7
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $b3
+	sfx_frames $04
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $0f
+	sfx_pulse2_command_flags_bits = $0f
+	sfx_pulse2_instrument $2e
+	sfx_pulse2_duty_cycle 1
+	sfx_pulse2_volume $d
+	sfx_pulse2_pitch_slide $7f
+	sfx_pulse2_note E_2
+	sfx_noise_command_flags $0d
+	sfx_noise_command_flags_bits = $0d
+	sfx_noise_instrument $37
+	sfx_noise_volume $6
+	sfx_noise_pitch_slide $7f
+	sfx_noise_note 9
+
+;3
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $b3
+	sfx_frames $03
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $0c
+	sfx_pulse2_command_flags_bits = $0c
+	sfx_pulse2_volume $f
+	sfx_pulse2_pitch_slide $ff
+	sfx_pulse2_note C#2
+	sfx_noise_command_flags $05
+	sfx_noise_command_flags_bits = $05
+	sfx_noise_instrument $2c
+	sfx_noise_volume $6
+	sfx_noise_note 13
+
+;4
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $b3
+	sfx_frames $04
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $0f
+	sfx_pulse2_command_flags_bits = $0f
+	sfx_pulse2_instrument $39
+	sfx_pulse2_duty_cycle 3
+	sfx_pulse2_volume $c
+	sfx_pulse2_pitch_slide $8a
+	sfx_pulse2_note A_1
+	sfx_noise_command_flags $0d
+	sfx_noise_command_flags_bits = $0d
+	sfx_noise_instrument $34
+	sfx_noise_volume $8
+	sfx_noise_pitch_slide $ff
+	sfx_noise_note 4
+
+;5
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $b3
+	sfx_frames $06
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $0c
+	sfx_pulse2_command_flags_bits = $0c
+	sfx_pulse2_volume $c
+	sfx_pulse2_pitch_slide $7f
+	sfx_pulse2_note G_1
+	sfx_noise_command_flags $0d
+	sfx_noise_command_flags_bits = $0d
+	sfx_noise_instrument $37
+	sfx_noise_volume $6
+	sfx_noise_pitch_slide $7f
+	sfx_noise_note 3
+
+;6
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $b3
+	sfx_frames $06
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $04
+	sfx_pulse2_command_flags_bits = $04
+	sfx_pulse2_volume $7
+	sfx_pulse2_note G_1
+	sfx_noise_command_flags $04
+	sfx_noise_command_flags_bits = $04
+	sfx_noise_volume $4
+	sfx_noise_note 3
+	sfx_end
