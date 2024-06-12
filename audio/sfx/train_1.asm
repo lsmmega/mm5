da2beaaa1282ae0bacce546b1e99109d802e16e8
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-12 11:31:56 +0800

    Update sfx

---
+++ audio/sfx/train_1.asm
@@ -0,0 +1,55 @@
+sfx_train_1:
+;1
+	sfx_priority $08
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $6e
+	sfx_frames $04
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $07
+	sfx_pulse2_command_flags_bits = $07
+	sfx_pulse2_instrument $40
+	sfx_pulse2_duty_cycle 3
+	sfx_pulse2_volume $8
+	sfx_pulse2_note C_3
+	sfx_noise_command_flags $0d
+	sfx_noise_command_flags_bits = $0d
+	sfx_noise_instrument $2b
+	sfx_noise_volume $e
+	sfx_noise_pitch_slide $0a
+	sfx_noise_note 6
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $6e
+	sfx_frames $04
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $08
+	sfx_pulse2_command_flags_bits = $08
+	sfx_pulse2_pitch_slide $93
+	sfx_pulse2_note C_3
+	sfx_noise_command_flags $08
+	sfx_noise_command_flags_bits = $08
+	sfx_noise_pitch_slide $05
+	sfx_noise_note 6
+
+;3
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $6e
+	sfx_frames $05
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $0c
+	sfx_pulse2_command_flags_bits = $0c
+	sfx_pulse2_volume $6
+	sfx_pulse2_pitch_slide $00
+	sfx_pulse2_note C_3
+	sfx_noise_command_flags $04
+	sfx_noise_command_flags_bits = $04
+	sfx_noise_volume $5
+	sfx_noise_note 6
+	sfx_end
