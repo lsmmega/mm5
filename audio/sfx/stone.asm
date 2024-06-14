a2509f6dc1b207182e8be13a9fb44045ce611a54
lsmmega <pogmegalsm@outlook.com>
2024-06-14 23:31:41 +0800

    Create stone.asm

---
+++ audio/sfx/stone.asm
@@ -0,0 +1,73 @@
+sfx_stone:
+;1
+	sfx_priority $09
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $78
+	sfx_global_transpose_note = $00
+	sfx_frames $04
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $07
+	sfx_pulse2_command_flags_bits = $07
+	sfx_pulse2_instrument $36
+	sfx_pulse2_duty_cycle 3
+	sfx_pulse2_volume $f
+	sfx_pulse2_note A_2
+	sfx_noise_command_flags $0d
+	sfx_noise_command_flags_bits = $0d
+	sfx_noise_instrument $2b
+	sfx_noise_volume $d
+	sfx_noise_pitch_slide $0a
+	sfx_noise_note 6
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $78
+	sfx_global_transpose_note = $00
+	sfx_frames $04
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $00
+	sfx_pulse2_command_flags_bits = $00
+	sfx_pulse2_note A_2
+	sfx_noise_command_flags $09
+	sfx_noise_command_flags_bits = $09
+	sfx_noise_instrument $2b
+	sfx_noise_pitch_slide $00
+	sfx_noise_note 6
+
+;3
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $11
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $0b
+	sfx_pulse2_command_flags_bits = $0b
+	sfx_pulse2_instrument $2d
+	sfx_pulse2_duty_cycle 2
+	sfx_pulse2_pitch_slide $5b
+	sfx_pulse2_note C_3
+	sfx_noise_command_flags $09
+	sfx_noise_command_flags_bits = $09
+	sfx_noise_instrument $28
+	sfx_noise_pitch_slide $5d
+	sfx_noise_note 11
+
+;4
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $8b
+	sfx_global_transpose_note = $00
+	sfx_frames $23
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_command_flags $04
+	sfx_noise_command_flags_bits = $04
+	sfx_noise_volume $4
+	sfx_noise_note 11
+	sfx_end
