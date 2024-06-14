81c9231b40a1c111bdede9fe0d3ecae1968bce1a
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-14 16:17:11 +0800

    Update sfx

---
+++ audio/sfx/metall_cannon.asm
@@ -0,0 +1,21 @@
+sfx_metall_cannon:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $0b
+	sfx_channel_flags $6
+	sfx_channel_flags_bits = $6
+	sfx_pulse2_command_flags $07
+	sfx_pulse2_command_flags_bits = $07
+	sfx_pulse2_instrument $28
+	sfx_pulse2_duty_cycle 2
+	sfx_pulse2_volume $7
+	sfx_pulse2_note G_4
+	sfx_triangle_command_flags $04
+	sfx_triangle_command_flags_bits = $04
+	sfx_triangle_fraction $ff
+	sfx_triangle_note G#5
+	sfx_end
