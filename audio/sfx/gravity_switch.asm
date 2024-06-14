19f475631750c8f9808e0b755e475f40f099a09d
lsmmega <pogmegalsm@outlook.com>
2024-06-14 23:31:39 +0800

    Create gravity_switch.asm

---
+++ audio/sfx/gravity_switch.asm
@@ -0,0 +1,18 @@
+sfx_gravity_switch:
+;1
+	sfx_priority $08
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $7f
+	sfx_global_transpose_note = $00
+	sfx_frames $1f
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $0f
+	sfx_pulse2_command_flags_bits = $0f
+	sfx_pulse2_instrument $25
+	sfx_pulse2_duty_cycle 3
+	sfx_pulse2_volume $f
+	sfx_pulse2_pitch_slide $d3
+	sfx_pulse2_note C#3
+	sfx_end
