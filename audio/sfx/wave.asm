08ef15bec8239d403ff22b5365f3c5512d536949
lsmmega <pogmegalsm@outlook.com>
2024-06-14 23:31:43 +0800

    Create wave.asm

---
+++ audio/sfx/wave.asm
@@ -0,0 +1,30 @@
+sfx_wave:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $07
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_command_flags $05
+	sfx_noise_command_flags_bits = $05
+	sfx_noise_instrument $36
+	sfx_noise_volume $f
+	sfx_noise_note 14
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $c1
+	sfx_global_transpose_note = $00
+	sfx_frames $12
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_command_flags $09
+	sfx_noise_command_flags_bits = $09
+	sfx_noise_instrument $28
+	sfx_noise_pitch_slide $26
+	sfx_noise_note 14
+	sfx_end
