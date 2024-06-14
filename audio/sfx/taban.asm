c230fe9dd4de70a993ce52e14e4b3c73a4d4cc0c
lsmmega <164604304+lsmmega@users.noreply.github.com>
2024-06-14 10:01:46 +0800

    Update sfx

---
+++ audio/sfx/taban.asm
@@ -0,0 +1,43 @@
+sfx_taban:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $b3
+	sfx_global_transpose_note = $00
+	sfx_frames $04
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $0d
+	sfx_pulse2_command_flags_bits = $0d
+	sfx_pulse2_instrument $40
+	sfx_pulse2_volume $d
+	sfx_pulse2_pitch_slide $7f
+	sfx_pulse2_note 
+	sfx_noise_command_flags $0d
+	sfx_noise_command_flags_bits = $0d
+	sfx_noise_instrument $2b
+	sfx_noise_volume $b
+	sfx_noise_pitch_slide $05
+	sfx_noise_note 6
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $b3
+	sfx_global_transpose_note = $00
+	sfx_frames $04
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $0d
+	sfx_pulse2_command_flags_bits = $0d
+	sfx_pulse2_instrument $40
+	sfx_pulse2_volume $d
+	sfx_pulse2_pitch_slide $2e
+	sfx_pulse2_note 
+	sfx_noise_command_flags $05
+	sfx_noise_command_flags_bits = $05
+	sfx_noise_instrument $2b
+	sfx_noise_volume $b
+	sfx_noise_note 7
+	sfx_end
