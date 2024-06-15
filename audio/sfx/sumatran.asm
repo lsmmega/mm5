696c8801c2e7e3b1da644a315ef94cd88dbd1fb8
lsmmega <pogmegalsm@outlook.com>
2024-06-15 22:42:11 +0800

    Create sumatran.asm

---
+++ audio/sfx/sumatran.asm
@@ -0,0 +1,42 @@
+sfx_sumatran:
+;1
+	sfx_priority $07
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $de
+	sfx_global_transpose_note = $00
+	sfx_frames $07
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $05
+	sfx_pulse2_command_flags_bits = $05
+	sfx_pulse2_instrument $26
+	sfx_pulse2_volume $6
+	sfx_pulse2_note C_2
+	sfx_noise_command_flags $05
+	sfx_noise_command_flags_bits = $05
+	sfx_noise_instrument $35
+	sfx_noise_volume $9
+	sfx_noise_note 0
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $18
+	sfx_channel_flags $a
+	sfx_channel_flags_bits = $a
+	sfx_pulse2_command_flags $0d
+	sfx_pulse2_command_flags_bits = $0d
+	sfx_pulse2_instrument $26
+	sfx_pulse2_volume $9
+	sfx_pulse2_pitch_slide $44
+	sfx_pulse2_note B_3
+	sfx_noise_command_flags $0d
+	sfx_noise_command_flags_bits = $0d
+	sfx_noise_instrument $27
+	sfx_noise_volume $e
+	sfx_noise_pitch_slide $1b
+	sfx_noise_note 4
+	sfx_end
