599b79c54c4aac31190abd414316a69ffbb47d6c
lsmmega <pogmegalsm@outlook.com>
2024-06-15 22:42:10 +0800

    Create gravity.asm

---
+++ audio/sfx/gravity.asm
@@ -0,0 +1,59 @@
+sfx_gravity:
+;1
+	sfx_priority $09
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $ff
+	sfx_global_transpose_note = $00
+	sfx_frames $0c
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $0f
+	sfx_pulse2_command_flags_bits = $0f
+	sfx_pulse2_instrument $2c
+	sfx_pulse2_duty_cycle 3
+	sfx_pulse2_volume $f
+	sfx_pulse2_pitch_slide $1f
+	sfx_pulse2_note E_4
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $dc
+	sfx_global_transpose_note = $00
+	sfx_frames $07
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $08
+	sfx_pulse2_command_flags_bits = $08
+	sfx_pulse2_pitch_slide $00
+	sfx_pulse2_note B_3
+
+;3
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $dc
+	sfx_global_transpose_note = $00
+	sfx_frames $06
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $00
+	sfx_pulse2_command_flags_bits = $00
+	sfx_pulse2_note C#5
+
+;4
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $1e
+	sfx_global_transpose_note = $00
+	sfx_frames $14
+	sfx_channel_flags $2
+	sfx_channel_flags_bits = $2
+	sfx_pulse2_command_flags $0f
+	sfx_pulse2_command_flags_bits = $0f
+	sfx_pulse2_instrument $09
+	sfx_pulse2_duty_cycle 3
+	sfx_pulse2_volume $e
+	sfx_pulse2_pitch_slide $00
+	sfx_pulse2_note A_7_
+	sfx_end
