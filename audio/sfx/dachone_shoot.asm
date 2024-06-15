90e59c62ab0be0e7eefdf8785a07bf4b985ad56a
lsmmega <pogmegalsm@outlook.com>
2024-06-15 22:42:06 +0800

    Create dachone_shoot.asm

---
+++ audio/sfx/dachone_shoot.asm
@@ -0,0 +1,28 @@
+sfx_dachone_shoot:
+;1
+	sfx_priority $08
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $bb
+	sfx_global_transpose_note = $00
+	sfx_frames $04
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_command_flags $01
+	sfx_noise_command_flags_bits = $01
+	sfx_noise_instrument $27
+	sfx_noise_note 10
+
+;2
+	sfx_global_flags $2
+	sfx_global_flags_bits = $2
+	sfx_global_note_sustain_length $59
+	sfx_global_transpose_note = $00
+	sfx_frames $19
+	sfx_channel_flags $8
+	sfx_channel_flags_bits = $8
+	sfx_noise_command_flags $01
+	sfx_noise_command_flags_bits = $01
+	sfx_noise_instrument $3e
+	sfx_noise_note 10
+	sfx_end
