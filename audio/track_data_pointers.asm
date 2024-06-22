864ebd87c665ddd71be775ab30a7e4cb36b009c7
lsmmega <pogmegalsm@outlook.com>
2024-06-22 17:43:55 +0800

    Create track_data_pointers.asm

---
+++ audio/track_data_pointers.asm
@@ -0,0 +1,172 @@
+;track_total
+	db (track_data_end-track_data_start)/2
+
+;instruments_pointers
+	dh instruments_data
+	dl instruments_data
+
+;music
+	track_data_start
+	dh music_gravity
+	dl music_gravity
+	dh music_stone
+	dl music_stone
+	dh music_wave
+	dl music_wave
+	dh music_star
+	dl music_star
+	dh music_crystal
+	dl music_crystal
+	dh music_gyro
+	dl music_gyro
+	dh music_charge
+	dl music_charge
+	dh music_napalm
+	dl music_napalm
+	dh music_dark
+	dl music_dark
+	dh music_wily
+	dl music_wily
+	dh music_boss
+	dl music_boss
+	dh music_opening
+	dl music_opening
+	dh music_ending
+	dl music_ending
+	dh music_stage_select
+	dl music_stage_select
+	dh music_game_start
+	dl music_game_start
+	dh music_dark_map
+	dl music_dark_map
+	dh music_wily_map
+	dl music_wily_map
+	dh music_stage_clear
+	dl music_stage_clear
+	dh music_game_over
+	dl music_game_over
+	dh music_pass_word
+	dl music_pass_word
+	dh music_get_weapon
+	dl music_get_weapon
+	dh music_all_stage_clear
+	dl music_all_stage_clear
+	dh music_last_boss
+	dl music_last_boss
+
+;sfx
+	dh sfx_lighting
+	dl sfx_lighting
+	dh sfx_routine_map
+	dl sfx_routine_map
+	dh sfx_ufo
+	dl sfx_ufo
+	dh sfx_shoot
+	dl sfx_shoot
+	dh sfx_landing
+	dl sfx_landing
+	dh sfx_damage_mm
+	dl sfx_damage_mm
+	dh sfx_death
+	dl sfx_death
+	dh sfx_shoot_recoil
+	dl sfx_shoot_recoil
+	dh sfx_pipe
+	dl sfx_pipe
+	dh sfx_half_charged_shoot
+	dl sfx_half_charged_shoot
+	dh sfx_fully_charged_shoot
+	dl sfx_fully_charged_shoot
+	dh sfx_charge_buster
+	dl sfx_charge_buster
+	dh sfx_charge_kick
+	dl sfx_charge_kick
+	dh sfx_1up
+	dl sfx_1up
+	dh sfx_boss_door
+	dl sfx_boss_door
+	dh sfx_gain_bar
+	dl sfx_gain_bar
+	dh sfx_change_weapon
+	dl sfx_change_weapon
+	dh sfx_close_menu
+	dl sfx_close_menu
+	dh sfx_open_menu
+	dl sfx_open_menu
+	dh sfx_damage_boss
+	dl sfx_damage_boss
+	dh sfx_destroy_enemy
+	dl sfx_destroy_enemy
+	dh sfx_explosion
+	dl sfx_explosion
+	dh sfx_teleport_in
+	dl sfx_teleport_in
+	dh sfx_password_wrong
+	dl sfx_password_wrong
+
+;music
+	dh music_fake_proto_whistle
+	dl music_fake_proto_whistle
+	dh music_proto_whistle
+	dl music_proto_whistle
+
+;sfx
+	dh sfx_lift_up
+	dl sfx_lift_up
+	dh sfx_life_slow
+	dl sfx_life_slow
+	dh sfx_train_1
+	dl sfx_train_1
+	dh sfx_train_2
+	dl sfx_train_2
+	dh sfx_teleport_out
+	dl sfx_teleport_out
+	dh sfx_power_muscler
+	dl sfx_power_muscler
+	dh sfx_taban
+	dl sfx_taban
+	dh sfx_teckyun
+	dl sfx_teckyun
+	dh sfx_yoku_block
+	dl sfx_yoku_block
+	dh sfx_water
+	dl sfx_water
+	dh sfx_enemy_falling
+	dl sfx_enemy_falling
+	dh sfx_metall_cannon
+	dl sfx_metall_cannon
+	dh sfx_metallk1000
+	dl sfx_metallk1000
+	dh sfx_beat
+	dl sfx_beat
+	dh sfx_gravity_switch
+	dl sfx_gravity_switch
+	dh sfx_water_wave
+	dl sfx_watet_wave
+	dh sfx_gyro_attack
+	dl sfx_gyro_attack
+	dh sfx_charge_up
+	dl sfx_charge_up
+	dh sfx_power_stone
+	dl sfx_power_stone
+	dh sfx_gravity_hold
+	dl sfx_gravity_hold
+	dh sfx_sumatran
+	dl sfx_sumatran
+	dh sfx_unused46
+	dl sfx_unused46
+	dh sfx_dachone_walking
+	dl sfx_dachone_walking
+	dh sfx_dachone_shoot
+	dl sfx_dachone_shoot
+
+;music
+	dh music_title
+	dl music_title
+	dh music_escape
+	dl music_escape
+
+;sfx
+	dh sfx_get_weapon_energy
+	dl sfx_get_weapon_energy
+	track_data_end
