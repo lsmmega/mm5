53988d03e3b7534114e75ebcfbc7419919e60387
lsmmega <pogmegalsm@outlook.com>
2025-10-13 12:35:48 +0800

    Create flags.asm

---
+++ constants/flags.asm
@@ -0,0 +1,39 @@
+;beat letter flags.
+	got_letter_m1 = 1 << 0
+	got_letter_e  = 1 << 1
+	got_letter_g  = 1 << 2
+	got_letter_a1 = 1 << 3
+	got_letter_m2 = 1 << 4
+	got_letter_a2 = 1 << 5
+	got_letter_n  = 1 << 6
+	got_letter_v  = 1 << 7
+
+;caslte stage clear flags.
+	dark_1_clear = 1 << 0
+	dark_2_clear = 1 << 1
+	dark_3_clear = 1 << 2
+	dark_4_clear = 1 << 3
+	wily_1_clear = 1 << 4
+	wily_2_clear = 1 << 5
+	wily_3_clear = 1 << 6
+	wily_4_clear = 1 << 7
+
+;rematch flags.
+	rematch_gravityman_done = 1 << 0
+	rematch_waveman_done    = 1 << 1
+	rematch_stoneman_done   = 1 << 2
+	rematch_gyroman_done    = 1 << 3
+	rematch_starman_done    = 1 << 4
+	rematch_chargeman_done  = 1 << 5
+	rematch_napalmman_done  = 1 << 6
+	rematch_crystalman_done = 1 << 7
+
+;regular stage clear flags.
+	stage_gravityman_clear = 1 << 0
+	stage_waveman_clear    = 1 << 1
+	stage_stoneman_clear   = 1 << 2
+	stage_gyroman_clear    = 1 << 3
+	stage_starman_clear    = 1 << 4
+	stage_chargeman_clear  = 1 << 5
+	stage_napalmman_clear  = 1 << 6
+	stage_crystalman_clear = 1 << 7
