fe17e623ebb6ae992cdf885eb340d4dd890895a1
lsmmega <pogmegalsm@outlook.com>
2025-12-24 22:41:56 +0800

    split mapset

---
+++ stages/stoneman/stoneman_mapset.asm
@@ -0,0 +1,399 @@
+;stoneman_mapset_00
+	.BYTE $00, $00, $00, $01, $01, $00, $00, $00
+	.BYTE $00, $00, $00, $01, $01, $00, $00, $00
+	.BYTE $00, $00, $00, $01, $01, $00, $00, $00
+	.BYTE $02, $02, $02, $03, $04, $02, $02, $02
+	.BYTE $05, $05, $05, $06, $06, $05, $05, $05
+	.BYTE $05, $05, $05, $06, $06, $05, $05, $05
+	.BYTE $07, $08, $09, $0A, $0B, $0C, $0D, $09
+	.BYTE $0E, $0E, $0F, $10, $11, $12, $0E, $0F
+
+;stoneman_mapset_01
+	.BYTE $00, $00, $01, $01, $01, $01, $00, $00
+	.BYTE $00, $00, $01, $01, $01, $01, $00, $00
+	.BYTE $00, $00, $01, $01, $01, $01, $00, $00
+	.BYTE $02, $02, $13, $03, $04, $13, $02, $02
+	.BYTE $05, $05, $06, $06, $06, $06, $05, $14
+	.BYTE $05, $05, $06, $15, $16, $16, $16, $17
+	.BYTE $0A, $0B, $0C, $18, $0E, $0E, $0E, $17
+	.BYTE $10, $11, $12, $0E, $0E, $0E, $0E, $17
+
+;stoneman_mapset_02
+	.BYTE $00, $00, $01, $01, $19, $1A, $1B, $1C
+	.BYTE $00, $00, $01, $01, $19, $1D, $1B, $1E
+	.BYTE $00, $00, $01, $01, $19, $1A, $1B, $1D
+	.BYTE $02, $02, $13, $1F, $20, $21, $22, $23
+	.BYTE $24, $25, $26, $27, $0E, $17, $28, $29
+	.BYTE $28, $29, $2A, $0E, $0E, $17, $28, $29
+	.BYTE $28, $29, $2A, $0E, $0E, $17, $28, $29
+	.BYTE $28, $29, $2A, $0E, $0E, $17, $28, $29
+
+;stoneman_mapset_03
+	.BYTE $2B, $2B, $2B, $2B, $1A, $1D, $2C, $1A
+	.BYTE $1E, $1E, $1E, $1B, $1E, $1E, $2C, $1B
+	.BYTE $1D, $1A, $1A, $2D, $2E, $2F, $0D, $1A
+	.BYTE $30, $31, $0D, $18, $0E, $0E, $0E, $1C
+	.BYTE $2A, $0E, $0E, $0E, $0E, $0E, $0E, $1C
+	.BYTE $2A, $0E, $0E, $0E, $0E, $0E, $0E, $1C
+	.BYTE $2A, $0E, $0E, $0E, $0E, $0E, $0E, $1C
+	.BYTE $2A, $0E, $0E, $0E, $0E, $0E, $0E, $1C
+
+;stoneman_mapset_04
+	.BYTE $0E, $0E, $0E, $32, $33, $34, $35, $36
+	.BYTE $37, $38, $38, $39, $1A, $1D, $1A, $1B
+	.BYTE $3A, $1E, $1B, $1E, $1E, $1E, $1E, $1B
+	.BYTE $3A, $1B, $1A, $2D, $2E, $2E, $2E, $2E
+	.BYTE $3B, $3C, $38, $3D, $2B, $2B, $0E, $0E
+	.BYTE $0E, $2C, $1A, $1D, $1A, $1D, $2B, $0E
+	.BYTE $0E, $0D, $0D, $3E, $2E, $2E, $3F, $0E
+	.BYTE $0E, $0E, $0E, $0E, $0E, $0E, $40, $0E
+
+;stoneman_mapset_05
+	.BYTE $41, $33, $42, $0E, $0E, $43, $0E, $0E
+	.BYTE $1D, $1C, $44, $0E, $45, $46, $1E, $0E
+	.BYTE $1E, $1B, $47, $0E, $1B, $46, $1B, $0E
+	.BYTE $48, $1A, $49, $4A, $4B, $1C, $1E, $0E
+	.BYTE $4C, $4D, $1A, $1D, $1E, $1D, $4E, $0E
+	.BYTE $0E, $4F, $1A, $1A, $1B, $1A, $50, $0E
+	.BYTE $0E, $51, $0D, $0D, $3E, $2E, $2E, $52
+	.BYTE $0E, $0E, $0E, $0E, $0E, $0E, $0E, $0E
+
+;stoneman_mapset_06
+	.BYTE $0E, $0E, $17, $28, $29, $2A, $17, $28
+	.BYTE $0E, $0E, $17, $28, $29, $2A, $17, $28
+	.BYTE $0E, $0E, $34, $35, $36, $41, $17, $28
+	.BYTE $0E, $0E, $1D, $1A, $1D, $1B, $17, $28
+	.BYTE $0E, $0E, $1A, $1C, $1A, $1B, $17, $28
+	.BYTE $0E, $1A, $1D, $1A, $1A, $1B, $17, $28
+	.BYTE $53, $21, $22, $23, $30, $2E, $17, $28
+	.BYTE $0E, $17, $28, $29, $2A, $0E, $17, $28
+
+;stoneman_mapset_07
+	.BYTE $12, $0E, $32, $33, $33, $0F, $10, $11
+	.BYTE $12, $38, $39, $1E, $1A, $54, $55, $56
+	.BYTE $12, $1E, $1E, $1B, $1E, $1E, $1E, $1E
+	.BYTE $12, $1B, $57, $1B, $1A, $2D, $48, $1A
+	.BYTE $12, $1E, $58, $2E, $31, $18, $59, $1B
+	.BYTE $12, $5A, $1D, $1B, $1A, $5B, $5C, $0D
+	.BYTE $12, $51, $0D, $0D, $5D, $5E, $0E, $0E
+	.BYTE $12, $0E, $0E, $0E, $0E, $43, $0E, $0E
+
+;stoneman_mapset_08
+	.BYTE $12, $0E, $0E, $5F, $0E, $5F, $0E, $0E
+	.BYTE $60, $38, $61, $5F, $0E, $5F, $33, $33
+	.BYTE $1E, $1E, $1E, $62, $63, $62, $1D, $1C
+	.BYTE $1D, $1A, $1A, $1C, $1B, $1C, $1A, $1A
+	.BYTE $1E, $1E, $1E, $1E, $1B, $1E, $1B, $1E
+	.BYTE $0D, $3E, $48, $1C, $1E, $1D, $1A, $1D
+	.BYTE $0E, $0E, $5C, $3E, $2E, $2E, $31, $0D
+	.BYTE $0E, $0E, $0E, $0E, $0E, $0E, $0E, $0E
+
+;stoneman_mapset_09
+	.BYTE $5F, $0E, $5F, $33, $33, $34, $35, $36
+	.BYTE $62, $38, $62, $1E, $1B, $1E, $1B, $1E
+	.BYTE $1A, $1E, $1D, $1A, $1A, $1D, $1A, $1A
+	.BYTE $1A, $1E, $1A, $1A, $1D, $64, $1D, $1A
+	.BYTE $1E, $1B, $1E, $15, $0D, $5F, $3E, $2E
+	.BYTE $1A, $2D, $31, $18, $0E, $5F, $0E, $0E
+	.BYTE $0D, $18, $0E, $0E, $0E, $5F, $0E, $0E
+	.BYTE $0E, $0E, $0E, $0E, $0E, $5F, $0E, $0E
+
+;stoneman_mapset_0a
+	.BYTE $41, $33, $42, $0E, $0E, $65, $17, $28
+	.BYTE $1B, $1E, $66, $0E, $0E, $65, $17, $28
+	.BYTE $1A, $1D, $67, $68, $38, $69, $17, $28
+	.BYTE $1A, $1A, $1E, $1E, $1A, $46, $17, $28
+	.BYTE $48, $1A, $1E, $1B, $1D, $46, $17, $28
+	.BYTE $59, $1E, $1B, $1B, $1E, $46, $17, $28
+	.BYTE $5C, $64, $1E, $2E, $2E, $6A, $17, $28
+	.BYTE $0E, $5F, $1C, $0E, $0E, $0E, $17, $28
+
+;stoneman_mapset_0b
+	.BYTE $12, $0E, $0E, $0E, $0E, $0F, $10, $11
+	.BYTE $12, $2B, $2B, $0E, $0E, $54, $6B, $56
+	.BYTE $12, $1B, $1A, $1D, $1D, $1A, $1D, $1A
+	.BYTE $12, $6C, $57, $6C, $6C, $6C, $6C, $6C
+	.BYTE $12, $1B, $6D, $6E, $6F, $08, $70, $1D
+	.BYTE $12, $71, $1D, $1A, $1A, $1D, $09, $0A
+	.BYTE $12, $72, $73, $74, $2E, $75, $0F, $10
+	.BYTE $12, $0E, $0E, $0E, $0E, $76, $0F, $10
+
+;stoneman_mapset_0c
+	.BYTE $12, $0E, $0E, $0E, $0E, $0E, $0E, $0E
+	.BYTE $60, $0E, $0E, $0E, $0E, $0E, $0E, $0E
+	.BYTE $1D, $0E, $0E, $77, $77, $0E, $0E, $1C
+	.BYTE $6C, $6C, $6C, $1B, $1B, $6C, $6C, $6C
+	.BYTE $1A, $1A, $1A, $1B, $1B, $1A, $1D, $1A
+	.BYTE $0B, $0C, $1D, $1B, $1B, $1A, $1A, $78
+	.BYTE $11, $12, $6F, $08, $78, $79, $7A, $0E
+	.BYTE $11, $12, $0E, $0E, $0E, $0E, $0E, $0E
+
+;stoneman_mapset_0d
+	.BYTE $34, $35, $36, $41, $34, $35, $36, $41
+	.BYTE $1C, $1A, $1D, $1E, $1A, $1A, $1D, $1A
+	.BYTE $1A, $1D, $1A, $1B, $1A, $1D, $1A, $1A
+	.BYTE $6C, $6C, $6C, $1E, $1E, $1E, $1B, $1E
+	.BYTE $1D, $1A, $1D, $1B, $1A, $1D, $1A, $1D
+	.BYTE $79, $7A, $6F, $08, $1D, $1C, $7B, $78
+	.BYTE $0E, $0E, $0E, $1E, $1A, $7B, $0E, $0E
+	.BYTE $0E, $0E, $0E, $6F, $08, $0E, $0E, $0E
+
+;stoneman_mapset_0e
+	.BYTE $0E, $0E, $0E, $0E, $0E, $0E, $2C, $0E
+	.BYTE $1D, $2B, $2B, $2B, $1C, $1E, $2C, $0E
+	.BYTE $1A, $1D, $1A, $1D, $1A, $1B, $7C, $0E
+	.BYTE $1E, $1E, $1B, $1E, $1E, $1E, $7D, $0E
+	.BYTE $1A, $1C, $1A, $1A, $1A, $1B, $7D, $0E
+	.BYTE $79, $7A, $09, $0A, $0B, $0C, $7E, $0E
+	.BYTE $0E, $0E, $0F, $10, $11, $12, $0E, $0E
+	.BYTE $0E, $0E, $0F, $10, $11, $12, $0E, $0E
+
+;stoneman_mapset_0f
+	.BYTE $0E, $1D, $2C, $1A, $0E, $0E, $0E, $0E
+	.BYTE $0E, $7B, $2C, $1D, $1A, $1A, $0E, $0E
+	.BYTE $0E, $0E, $7B, $70, $1A, $1A, $1C, $7F
+	.BYTE $0E, $0E, $0E, $0E, $7B, $70, $1A, $7F
+	.BYTE $80, $1D, $1A, $2B, $1A, $1A, $1C, $81
+	.BYTE $80, $1A, $78, $79, $79, $79, $82, $0E
+	.BYTE $83, $1A, $1A, $1C, $1A, $1A, $1D, $0E
+	.BYTE $72, $07, $07, $07, $07, $07, $84, $0E
+
+;stoneman_mapset_10
+	.BYTE $0E, $0E, $0E, $0E, $0E, $0E, $0E, $0E
+	.BYTE $0E, $2B, $2B, $2B, $2B, $2B, $2B, $0E
+	.BYTE $0E, $1A, $1D, $1A, $1A, $1D, $1A, $0E
+	.BYTE $0E, $1D, $1A, $1C, $1A, $1A, $1D, $0E
+	.BYTE $79, $7A, $1A, $1A, $1D, $1A, $1A, $0E
+	.BYTE $0E, $0E, $7B, $85, $1A, $85, $86, $0E
+	.BYTE $0E, $0E, $0E, $87, $1C, $87, $88, $0E
+	.BYTE $0E, $0E, $0E, $87, $1C, $87, $88, $0E
+
+;stoneman_mapset_11
+	.BYTE $0E, $0E, $89, $01, $01, $01, $01, $01
+	.BYTE $0E, $0E, $89, $01, $01, $01, $01, $01
+	.BYTE $0E, $1C, $89, $01, $01, $01, $01, $01
+	.BYTE $0E, $1C, $89, $01, $01, $01, $8A, $01
+	.BYTE $0E, $1C, $89, $8A, $8B, $8C, $8D, $01
+	.BYTE $0E, $1C, $89, $8E, $8F, $90, $91, $92
+	.BYTE $0E, $1C, $93, $94, $6F, $08, $0E, $0E
+	.BYTE $0E, $1C, $7D, $0E, $0E, $0E, $0E, $0E
+
+;stoneman_mapset_12
+	.BYTE $01, $01, $01, $01, $01, $01, $01, $01
+	.BYTE $01, $01, $01, $01, $01, $01, $01, $01
+	.BYTE $01, $01, $01, $01, $01, $01, $8A, $01
+	.BYTE $01, $01, $95, $01, $8A, $01, $01, $01
+	.BYTE $8A, $96, $97, $98, $01, $8B, $8C, $8D
+	.BYTE $99, $9A, $9B, $9C, $9D, $8F, $90, $9E
+	.BYTE $0E, $0E, $9F, $9F, $A0, $A1, $06, $9B
+	.BYTE $0E, $0E, $9F, $9F, $A0, $A1, $06, $9F
+
+;stoneman_mapset_13
+	.BYTE $01, $01, $01, $01, $01, $01, $01, $01
+	.BYTE $01, $01, $01, $01, $01, $01, $01, $01
+	.BYTE $01, $01, $01, $01, $01, $8A, $01, $01
+	.BYTE $01, $8A, $95, $01, $01, $01, $01, $01
+	.BYTE $8A, $96, $97, $98, $01, $8B, $8C, $8D
+	.BYTE $98, $A2, $A3, $A4, $9D, $8F, $90, $9E
+	.BYTE $9C, $9F, $A3, $9B, $9C, $A5, $A6, $9B
+	.BYTE $9F, $9F, $A3, $9F, $9F, $9F, $9F, $9F
+
+;stoneman_mapset_14
+	.BYTE $01, $01, $01, $01, $01, $01, $01, $01
+	.BYTE $01, $01, $01, $01, $01, $01, $01, $01
+	.BYTE $01, $01, $01, $01, $8A, $01, $01, $01
+	.BYTE $8A, $01, $01, $95, $01, $A7, $01, $01
+	.BYTE $01, $8A, $96, $97, $A8, $A9, $AA, $8A
+	.BYTE $98, $A2, $AB, $AC, $AD, $AE, $9B, $AF
+	.BYTE $9C, $9F, $A0, $9F, $9F, $B0, $9F, $9F
+	.BYTE $9F, $9F, $A0, $9F, $9F, $9F, $9F, $9F
+
+;stoneman_mapset_15
+	.BYTE $19, $0E, $0E, $0E, $0E, $0E, $43, $0E
+	.BYTE $19, $1A, $1C, $1A, $1D, $1A, $46, $0E
+	.BYTE $19, $1D, $1A, $1A, $1A, $1D, $46, $0E
+	.BYTE $19, $1A, $1D, $1A, $1D, $1A, $46, $0E
+	.BYTE $19, $78, $79, $79, $79, $79, $B1, $0E
+	.BYTE $9C, $0E, $0E, $0E, $0E, $0E, $0E, $0E
+	.BYTE $9F, $0E, $0E, $0E, $0E, $0E, $0E, $0E
+	.BYTE $9F, $0E, $0E, $0E, $0E, $0E, $0E, $0E
+
+;stoneman_mapset_16
+	.BYTE $0E, $0E, $B2, $B2, $B3, $89, $01, $01
+	.BYTE $0E, $B2, $B2, $0E, $80, $89, $B4, $95
+	.BYTE $0E, $1C, $0E, $80, $1C, $89, $96, $97
+	.BYTE $0E, $0E, $0E, $72, $B5, $B6, $AB, $A3
+	.BYTE $0E, $0E, $0E, $0E, $72, $B7, $A0, $A3
+	.BYTE $0E, $0E, $0E, $0E, $0E, $B8, $B9, $BA
+	.BYTE $0E, $0E, $0E, $0E, $0E, $0E, $43, $0E
+	.BYTE $0E, $0E, $0E, $0E, $0E, $0E, $43, $0E
+
+;stoneman_mapset_17
+	.BYTE $01, $01, $01, $01, $B4, $01, $01, $8B
+	.BYTE $01, $01, $01, $8B, $8C, $8D, $8E, $8F
+	.BYTE $BB, $01, $8E, $8F, $90, $BC, $A2, $A5
+	.BYTE $A4, $9D, $BD, $A1, $BE, $A0, $9F, $9F
+	.BYTE $BE, $A0, $A0, $BF, $A0, $A0, $9F, $9F
+	.BYTE $AF, $9C, $A0, $A0, $A0, $A0, $9F, $9F
+	.BYTE $9F, $9F, $A0, $A0, $A0, $A0, $9F, $9F
+	.BYTE $9F, $9F, $A0, $A0, $A0, $A0, $9F, $9F
+
+;stoneman_mapset_18
+	.BYTE $8C, $8D, $01, $01, $B4, $19, $0E, $0E
+	.BYTE $90, $9E, $BB, $01, $A7, $19, $0E, $0E
+	.BYTE $A6, $06, $A4, $A8, $A9, $C0, $0E, $0E
+	.BYTE $9F, $A2, $06, $A4, $AE, $C1, $0E, $0E
+	.BYTE $9F, $9F, $A2, $06, $A4, $C1, $C2, $C3
+	.BYTE $9F, $9F, $9F, $A2, $06, $C1, $1C, $1C
+	.BYTE $9F, $9F, $9F, $9F, $06, $C1, $C4, $73
+	.BYTE $9F, $9F, $9F, $9F, $06, $C1, $0E, $0E
+
+;stoneman_mapset_19
+	.BYTE $C5, $1C, $1C, $1C, $1D, $1C, $C6, $0E
+	.BYTE $C7, $1D, $1C, $1A, $1A, $1C, $C6, $0E
+	.BYTE $0E, $1A, $1D, $1A, $1A, $1A, $C8, $0E
+	.BYTE $80, $1A, $1A, $1A, $1A, $1D, $1C, $0E
+	.BYTE $C9, $1A, $1D, $1A, $1D, $1A, $1A, $0E
+	.BYTE $1C, $1C, $1C, $1A, $1A, $1C, $1D, $0E
+	.BYTE $73, $B5, $1C, $1A, $1D, $1A, $1D, $0E
+	.BYTE $0E, $80, $1D, $1C, $1C, $1D, $1C, $0E
+
+;stoneman_mapset_1a
+	.BYTE $0E, $43, $CA, $0E, $0E, $0E, $0E, $0E
+	.BYTE $0E, $43, $CB, $CC, $CC, $CD, $0E, $0E
+	.BYTE $0E, $CE, $CF, $CF, $CF, $CB, $CD, $0E
+	.BYTE $0E, $D0, $CF, $CF, $CF, $CF, $CB, $0E
+	.BYTE $0E, $43, $CF, $CF, $CF, $CF, $D1, $D2
+	.BYTE $0E, $72, $08, $D3, $D4, $D5, $D6, $0E
+	.BYTE $0E, $0E, $0E, $0E, $0E, $0E, $7D, $0E
+	.BYTE $0E, $0E, $0E, $0E, $0E, $0E, $7D, $0E
+
+;stoneman_mapset_1b
+	.BYTE $0E, $0E, $0E, $0E, $0E, $0E, $0E, $0E
+	.BYTE $0E, $0E, $0E, $0E, $0E, $0E, $0E, $0E
+	.BYTE $0E, $0E, $0E, $0E, $0E, $0E, $0E, $0E
+	.BYTE $0E, $CC, $CC, $CC, $CC, $CC, $0E, $0E
+	.BYTE $79, $D4, $D5, $CF, $CF, $CF, $0E, $0E
+	.BYTE $0E, $0E, $0E, $7B, $D7, $CF, $0E, $0E
+	.BYTE $0E, $0E, $0E, $0E, $0E, $7B, $0E, $0E
+	.BYTE $0E, $0E, $0E, $0E, $0E, $0E, $0E, $0E
+
+;stoneman_mapset_1c
+	.BYTE $0E, $0E, $0E, $80, $89, $01, $01, $01
+	.BYTE $0E, $0E, $0E, $80, $89, $01, $01, $01
+	.BYTE $0E, $0E, $80, $1D, $89, $D8, $D9, $DA
+	.BYTE $0E, $0E, $80, $1A, $DB, $A0, $A0, $A0
+	.BYTE $0E, $80, $1D, $1A, $DB, $A0, $A0, $DC
+	.BYTE $0E, $80, $1A, $1C, $DD, $DE, $DF, $0E
+	.BYTE $0E, $E0, $E1, $74, $0E, $0E, $0E, $0E
+	.BYTE $0E, $43, $E2, $0E, $0E, $0E, $0E, $0E
+
+;stoneman_mapset_1d
+	.BYTE $01, $01, $01, $01, $01, $01, $01, $01
+	.BYTE $01, $01, $01, $01, $01, $01, $01, $01
+	.BYTE $DA, $E3, $E3, $D9, $DA, $DA, $E4, $D9
+	.BYTE $E5, $DE, $A0, $E6, $E7, $E7, $E7, $E8
+	.BYTE $9F, $9F, $A0, $A0, $A0, $A0, $A0, $A0
+	.BYTE $9F, $9F, $E5, $DE, $A0, $A0, $A0, $A0
+	.BYTE $9F, $9F, $9F, $9F, $E9, $EA, $9C, $A2
+	.BYTE $9F, $9F, $9F, $9F, $EB, $EC, $9F, $9F
+
+;stoneman_mapset_1e
+	.BYTE $01, $01, $01, $01, $01, $01, $01, $01
+	.BYTE $D8, $D8, $E3, $D9, $DA, $DA, $D8, $D8
+	.BYTE $A0, $ED, $EE, $A0, $A0, $A0, $A0, $A0
+	.BYTE $A0, $A0, $A0, $A0, $ED, $EF, $EE, $A0
+	.BYTE $A0, $A0, $A0, $A0, $A0, $A0, $ED, $EE
+	.BYTE $A2, $A0, $A2, $A0, $A0, $A0, $A0, $A0
+	.BYTE $9F, $E9, $9F, $A2, $A2, $E9, $E9, $9B
+	.BYTE $9F, $EB, $9F, $9F, $9F, $EB, $EB, $9F
+
+;stoneman_mapset_1f
+	.BYTE $F0, $0E, $0E, $0E, $0E, $0E, $0E, $0E
+	.BYTE $F0, $CC, $2B, $0E, $0E, $0E, $0E, $0E
+	.BYTE $F1, $CF, $CF, $2B, $2B, $0E, $0E, $0E
+	.BYTE $F1, $CF, $CF, $CF, $CF, $2B, $2B, $0E
+	.BYTE $F1, $CF, $CF, $CF, $CF, $CF, $CF, $F2
+	.BYTE $F1, $CF, $CF, $6F, $F3, $CF, $CF, $F4
+	.BYTE $AF, $AF, $9C, $0E, $0E, $6F, $07, $07
+	.BYTE $9F, $9F, $9F, $0E, $0E, $0E, $0E, $0E
+
+;stoneman_mapset_20
+	.BYTE $0E, $0E, $9F, $0E, $0E, $9F, $0E, $0E
+	.BYTE $0E, $0E, $9F, $0E, $0E, $9F, $0E, $0E
+	.BYTE $0E, $0E, $9F, $0E, $0E, $9F, $0E, $0E
+	.BYTE $0E, $0E, $9F, $0E, $0E, $9F, $0E, $0E
+	.BYTE $2B, $2B, $F5, $2B, $2B, $F5, $2B, $F2
+	.BYTE $CF, $CF, $CF, $CF, $CF, $CF, $CF, $F4
+	.BYTE $07, $07, $A2, $07, $07, $A2, $07, $07
+	.BYTE $0E, $0E, $9F, $0E, $0E, $9F, $0E, $0E
+
+;stoneman_mapset_21
+	.BYTE $F6, $F7, $F6, $F7, $F7, $F6, $F7, $F6
+	.BYTE $F8, $F9, $CC, $F9, $F9, $CC, $F9, $FA
+	.BYTE $FB, $1B, $CF, $1B, $1B, $CF, $1B, $FC
+	.BYTE $FB, $1B, $CF, $1B, $1B, $CF, $1B, $FC
+	.BYTE $FD, $1B, $CF, $1B, $1B, $CF, $1B, $FC
+	.BYTE $CF, $1B, $CF, $1B, $1B, $CF, $1B, $FC
+	.BYTE $A2, $07, $A2, $07, $07, $A2, $07, $A2
+	.BYTE $9F, $0E, $9F, $0E, $0E, $9F, $0E, $9F
+
+;stoneman_mapset_22
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+
+;stoneman_mapset_23
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+
+;stoneman_mapset_24
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+
+;stoneman_mapset_25
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+
+;stoneman_mapset_26
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+
+;stoneman_mapset_27
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
+	.BYTE $1C, $1C, $1C, $1C, $1C, $1C, $1C, $1C
