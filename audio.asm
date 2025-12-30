d12aa393a81006d15e0dbea65910404533f26475
lsmmega <pogmegalsm@outlook.com>
2025-12-30 22:00:21 +0800

    sfx enemy hit done

--- audio.asm
+++ audio.asm
@@ -46,6 +46,7 @@
 .INCLUDE "audio/sfx/select.asm"
 .INCLUDE "audio/sfx/confirm.asm"
 .INCLUDE "audio/sfx/open_menu.asm"
+.INCLUDE "audio/sfx/enemy_hit.asm"
 .INCBIN  "audio/audio_0.bin"
 .INCLUDE "audio/music/fake_protoman_whistle.asm"
 .INCLUDE "audio/music/protoman_whistle.asm"
