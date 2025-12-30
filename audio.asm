1747f848b82e4f43418ea51f5a9ed3b3353060a1
lsmmega <pogmegalsm@outlook.com>
2025-12-30 21:46:43 +0800

    sfx confirm done

--- audio.asm
+++ audio.asm
@@ -44,6 +44,7 @@
 .INCLUDE "audio/sfx/door.asm"
 .INCLUDE "audio/sfx/refill.asm"
 .INCLUDE "audio/sfx/select.asm"
+.INCLUDE "audio/sfx/confirm.asm"
 .INCBIN  "audio/audio_0.bin"
 .INCLUDE "audio/music/fake_protoman_whistle.asm"
 .INCLUDE "audio/music/protoman_whistle.asm"
