1e1bdf804b13b98e25d4f1c698ef731d6f350910
lsmmega <pogmegalsm@outlook.com>
2026-03-11 22:27:25 +0800

    Update track.asm

--- constants/track.asm
+++ constants/track.asm
@@ -76,4 +76,12 @@
 	track_title
 	track_escape
 	track_get_weapon_energy
+	;init_audio         ;F0, F8
+	;init_sfx           ;F1, F9
+	;init_music         ;F2, FA
+	;pause_music        ;F3, FB
+	;play_music         ;F4, FC
+	;music_fade_in      ;F5, FD
+	;music_fade_out     ;F6, FE
+	;set_sfx_pitch_tune ;F7, FF
 .ENDENUM
